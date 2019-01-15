0aa792acf7e7519ae154e725c07be828af0ffec9
mbalhamer <mbalhamer@alsourayia.com.sa>
2019-01-15 12:52:43 +0300

    mb124444

--- app/src/main/java/com/sff/sffapp/FollowUp/FollowUp.java
+++ app/src/main/java/com/sff/sffapp/FollowUp/FollowUp.java
@@ -37,7 +37,7 @@ public class FollowUp extends AppCompatActivity implements ConnectionInterface, 
     String currFragment ;
     public    Toolbar myToolbar;
     public Toolbar myFooterToolbar ;
-    String mb3444;
+    String mb3444ddd;
     TextView rem_val_desc ;
 
     @Override
